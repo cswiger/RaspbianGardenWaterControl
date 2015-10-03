cecb2f1cd543f8107507412a023e1b88b3619780
Chuck <cswiger@swigerco.com>
2015-10-03 12:31:12 -0400

    remove info

--- weather.py
+++ weather.py
@@ -2,9 +2,9 @@ from urllib.request import urlopen
 from json import loads
 import ssl
 
-api_key = "e366469ed8d5bd01b9d4a6cb8ed489ae"
-lat = "38.353664"
-long = "-81.687318"
+api_key = "your forecast.io api key here"
+lat = "37.8267"
+long = "-122.423"
 
 def weather():
    url = urlopen("https://api.forecast.io/forecast/" + api_key + "/" + lat + "," + long)
